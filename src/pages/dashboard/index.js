91ecc70b9a2b6c1e2c40dc8c03d53ad3186875cb
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-08-09 10:20:04 -0300

    Update index.js

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,13 +1,43 @@
-import React from 'react';
-import './styles.css'; 
+import React, { useState } from 'react';
 
-const Dashboard = () => {
+const TodoDashboard = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
   return (
-    <div className="body">
-      <div className="center-box">
+    <div>
+      <h1>Todo List Dashboard</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a new task"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add Task</button>
       </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default Dashboard;
+export default TodoDashboard;
