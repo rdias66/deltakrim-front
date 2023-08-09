ac869b41b00687b15472e1f042fc70d5db7bea79
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-08-09 10:30:05 -0300

    Update index.js
    not tested

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -2,12 +2,31 @@ import React, { useState } from 'react';
 
 const TodoDashboard = () => {
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
+  const [newTask, setNewTask] = useState({
+    title: '',
+    description: '',
+    enteringDate: '',
+    dueDate: '',
+    state: ''
+  });
 
   const addTask = () => {
-    if (newTask.trim() !== '') {
-      setTasks([...tasks, newTask]);
-      setNewTask('');
+    if (newTask.title.trim() !== '') {
+      const now = new Date();
+      const nowFormatted = now.toISOString();
+      const taskToAdd = { 
+        ...newTask, 
+        enteringDate :nowFormatted
+      };
+      
+      setTasks([...tasks, taskToAdd]);
+      setNewTask({
+        title: '',
+        description: '',
+        enteringDate: '',
+        dueDate: '',
+        state: ''
+      });
     }
   };
 
@@ -19,19 +38,36 @@ const TodoDashboard = () => {
   return (
     <div>
       <h1>Todo List Dashboard</h1>
-      <div>
+      <form>
+        {/* Input fields for task details */}
         <input
           type="text"
-          placeholder="Enter a new task"
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Title"
+          value={newTask.title}
+          onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
         />
-        <button onClick={addTask}>Add Task</button>
-      </div>
+         <input
+          type="text"
+          placeholder="Description"
+          value={newTask.description}
+          onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
+        />
+          <input
+          type="text"
+          placeholder="Due date"
+          value={newTask.dueDate}
+          onChange={(e) => setNewTask({ ...newTask, dueDate: e.target.value })}
+        />
+     
+        <button type="button" onClick={addTask}>Add Task</button>
+      </form>
       <ul>
         {tasks.map((task, index) => (
           <li key={index}>
-            {task}
+            <h3>{task.title}</h3>
+            <p>{task.description}</p>
+            <p>Due: {task.dueDate}</p>
+            {/* Display more attributes */}
             <button onClick={() => deleteTask(index)}>Delete</button>
           </li>
         ))}
